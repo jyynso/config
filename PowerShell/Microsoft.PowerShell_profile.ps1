be358c62a230a130706dd6d49bc0fe78b091c40e
jyynso <ecwt26@gmail.com>
2026-05-30 09:53:50 +0800

    pwsh

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,33 @@
+#make pwsh prompt really nice
+oh-my-posh init pwsh --config 'C:\Users\ecwt\Documents\PowerShell\takuyaGruv.omp.json' | Invoke-Expression
+
+#username
+$env:USERNAME = "jyynso"
+#Aliases
+Set-Alias ff fastfetch.exe
+Set-Alias vim nvim
+Set-Alias g git
+Set-Alias adb .\adb
+Set-Alias komo komorebicStart
+Set-Alias komos komorebicStop
+Set-Alias komor komorebicRestart
+Set-Alias lg lazygit
+
+#Functions cause pwsh is a bitch
+
+function komorebicStart {
+	komorebic start --whkd 
+}
+
+function komorebicStop {
+	komorebic stop --whkd
+}
+
+function komorebicRestart {
+	komorebic stop; komorebic start --whkd
+}
+
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
