396a2809ff676c0a8877c97572803eebd82a29ee
unknown <ecwt26@gmail.com>
2026-06-01 15:55:50 +0800

    fix center window issue flickering

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,11 +1,11 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 local initial_cols = 78
-local initial_rows = 23
+local initial_rows = 19
 
-config.default_prog = { 'pwsh.exe', '-NoLogo' }
+-- appearance
 config.color_scheme = 'GruvboxDark' 
-config.font_size = 12
+config.font_size = 11
 config.hide_tab_bar_if_only_one_tab = true
 config.enable_tab_bar = true
 config.use_fancy_tab_bar = false
@@ -14,9 +14,10 @@ config.window_decorations = "RESIZE"
 config.font = wezterm.font({ family = "BlexMono Nerd Font" })
 config.front_end = "WebGpu"
 config.animation_fps = 60
-config.window_background_opacity = 0.85
+config.window_background_opacity = 0.80
 config.initial_cols = initial_cols
 config.initial_rows = initial_rows
+
 config.window_padding = {
     left = 30,
     right = 30,
@@ -24,6 +25,10 @@ config.window_padding = {
     bottom = 30,
 }
 
+-- default shell 
+config.default_prog = { 'pwsh.exe', '-NoLogo' }
+
+-- wsl on f
 config.keys = {
     {
         key = 'f',
@@ -40,6 +45,7 @@ config.keys = {
     },
 }
 
+-- shows pwsh on the tab instead of full path
 wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
     local title = tab.active_pane.title
 
@@ -52,6 +58,7 @@ wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_wid
     }
 end)
 
+-- center window on launch only works for 125% scale
 wezterm.on('gui-startup', function(cmd)
     local screen = wezterm.gui.screens().main
     cmd = cmd or {}
@@ -66,7 +73,5 @@ wezterm.on('gui-startup', function(cmd)
 
     wezterm.mux.spawn_window(cmd)
 end)
-
-
 
 return config
