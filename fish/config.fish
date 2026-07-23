f8bccc62fbde5c97d6787210141a28af396983bc
jyynso <ecwt26@gmail.com>
2026-07-23 13:47:10 +0800

    fih

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,5 @@
 set fish_greeting ""
 
-set -gx TERM xterm-256color
-
 alias vim nvim
 alias g git
 alias lg lazygit
@@ -9,4 +7,4 @@ alias ff fastfetch
 alias ll 'eza -l --icons'
 alias la 'eza -la --icons'
 
-set -gx PATH $HOME/.local/bin $HOME/bin $PATH
+fish_add_path $HOME/.local/bin $HOME/bin
